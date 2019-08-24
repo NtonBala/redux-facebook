7f64a9944f2c8f988997e640fa0fb8d0cfba7a3e
Anton Balashov <balashov.anton.1983@gmail.com>
2019-08-24 14:32:32 +0300

    [L-3] Refactor store.js

--- source/init/store.js
+++ source/init/store.js
@@ -1,39 +1,11 @@
 // Core
-import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
 // Reducer
 import { rootReducer } from './rootReducer';
 
-const logger = createLogger({
-    duration:  true,
-    collapsed: true,
-    colors:    {
-        title:     () => '#1398FE',
-        prevState: () => '#1C5FAF',
-        action:    () => '#149945',
-        nextState: () => '#A47104',
-        error:     () => '#FF0005',
-    },
-});
-
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const composeEnhancers = __DEV__ && devTools ? devTools : compose;
-
-const middleware = [
-    thunk
-];
-
-if (__DEV__) {
-    middleware.push(
-        logger
-    );
-}
-
-const enhancer = composeEnhancers(
-    applyMiddleware(...middleware)
-);
+// Enhancer
+import { enhancer } from './middleware/core';
 
 export const store = createStore(
     rootReducer,
