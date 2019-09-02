c1c95af726b8a92e95d83a005167871d12911945
Anton Balashov <anton.b@coaxsoft.com>
2019-09-02 18:21:54 +0300

    [L-4] Prepared structure for redux-saga

---
+++ source/bus/posts/saga/watchers.js
@@ -0,0 +1,18 @@
+// Core
+import { takeEvery, all, call } from 'redux-saga/effects';
+
+// Types
+import { CREATE_POST_ASYNC } from '../types';
+
+// Warkers
+import { createPost } from './workers';
+
+function* watchCreatePost () {
+    yield takeEvery(CREATE_POST_ASYNC, createPost);
+}
+
+export function* watchPosts () {
+    yield all([
+        call(watchCreatePost)
+    ]);
+}
