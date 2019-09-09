d36eaf54d7c6f814be23658537c0e232b004f93b
Anton Balashov <balashov.anton.1983@gmail.com>
2019-09-09 16:45:05 +0300

    [L-4] Add Spinner functionality

---
+++ source/bus/ui/actions.js
@@ -0,0 +1,16 @@
+// Types
+import { types } from './types';
+
+export const uiActions = {
+    startFetching: () => {
+        return {
+            type: types.START_FETCHING,
+        };
+    },
+
+    stopFetching: () => {
+        return {
+            type: types.STOP_FETCHING,
+        };
+    },
+};
