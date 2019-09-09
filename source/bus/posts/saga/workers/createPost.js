ce25c117a4cda2d5f5daf0b6c6c96ba79a4db71d
Anton Balashov <balashov.anton.1983@gmail.com>
2019-09-09 14:30:43 +0300

    [L-4] Refactor posts domain types & actions structure

--- source/bus/posts/saga/workers/createPost.js
+++ source/bus/posts/saga/workers/createPost.js
@@ -3,11 +3,11 @@ import { put, apply } from 'redux-saga/effects';
 
 // Instruments
 import { api } from '../../../../REST';
-import { createPost as createPostAC } from '../../actions';
+import { postsActions } from '../../actions';
 
 export function* createPost ({ payload: comment }) {
     const response = yield apply(api, api.posts.create, [comment]);
     const result = yield apply(response, response.json);
 
-    yield put(createPostAC(result.data));
+    yield put(postsActions.createPost(result.data));
 }
