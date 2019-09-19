771419a7335190d8212407651068e69318acdedd
Anton Balashov <balashov.anton.1983@gmail.com>
2019-09-19 21:18:25 +0300

    [L-5] Implement signup API call

--- source/REST/api.js
+++ source/REST/api.js
@@ -2,6 +2,18 @@
 import { MAIN_URL, groupId } from './config';
 
 export const api = {
+    auth: {
+        signup (userInfo) {
+            return fetch(`${MAIN_URL}/user/${groupId}`, {
+                method:  'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userInfo),
+            });
+        },
+    },
+
     posts: {
         fetch () {
             return fetch(`${MAIN_URL}/feed`, {
@@ -11,14 +23,14 @@ export const api = {
                 },
             });
         },
-        create (data) {
+        create (comment) {
             return fetch(`${MAIN_URL}/feed`, {
                 method:  'POST',
                 headers: {
                     'x-no-auth':    groupId,
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ comment: data }),
+                body: JSON.stringify({ comment }),
             });
         },
     },
